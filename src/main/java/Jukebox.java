3c2dae9f6fabfb1511ca0362d5ba0a6d56411e7c
Phung SQ <phungsiqi@gmail.com>
2026-08-19 22:16:40 +0800

    feat: add echo

--- src/main/java/Jukebox.java
+++ src/main/java/Jukebox.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
+
 public class Jukebox {
     public static void main(String[] args) {
         String chatbotName = "jukebox";
+        Scanner sc = new Scanner(System.in);
         // Personality: weirdo
         String greeting = String.format("Hoi hoi im %s nice to meet you whaddya need", chatbotName);
         System.out.println(greeting);
-        Jukebox.exit();
+        while (true) {
+            echo(sc);
+        }
     }
 
-    public static void exit() {
+    private static void exit() {
         System.out.println("gooooooooddbyyeee seeeeee youuuuuuuu");
+        System.exit(0);
+    }
+
+    private static void echo(Scanner sc) {
+        String inp = sc.nextLine();
+        if (inp.equals("bye")) {
+            Jukebox.exit();
+        } else {
+            System.out.println(inp);
+        }
     }
 }
