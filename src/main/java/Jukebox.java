83dca23bb8d1099a0acba196aa34ac3350407bd0
Phung SQ <phungsiqi@gmail.com>
2026-08-20 22:39:12 +0800

    refactor: use enum

--- src/main/java/Jukebox.java
+++ src/main/java/Jukebox.java
@@ -23,6 +23,22 @@ public class Jukebox {
     private static final Pattern EVENT_PATTERN =
             Pattern.compile("^event\\s+(.+?)\\s+/from\\s+(.+?)\\s+/to\\s+(.+)$");
 
+    enum Action {
+        BYE, LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, UNKNOWN;
+
+        public static Action fromInput(String inp) {
+            if (inp.equals("bye")) return BYE;
+            if (inp.equals("list")) return LIST;
+            if (inp.startsWith("mark")) return MARK;
+            if (inp.startsWith("unmark")) return UNMARK;
+            if (inp.startsWith("todo")) return TODO;
+            if (inp.startsWith("deadline")) return DEADLINE;
+            if (inp.startsWith("event")) return EVENT;
+            if (inp.startsWith("delete")) return DELETE;
+            return UNKNOWN;
+        }
+    }
+
     public static void main(String[] args) {
         String chatbotName = "jukebox";
         Scanner sc = new Scanner(System.in);
@@ -30,7 +46,7 @@ public class Jukebox {
         String greeting = String.format("Hoi hoi im %s nice to meet you :333", chatbotName);
         System.out.println(greeting);
         while (true) {
-            echo(sc);
+            chat(sc);
         }
     }
 
@@ -43,84 +59,106 @@ public class Jukebox {
         System.exit(0);
     }
 
-    private static void echo(Scanner sc) {
-        String inp = sc.nextLine();
-        if (inp.equals("bye")) {
-            Jukebox.exit();
-        } else if (inp.equals("list")) {
-            for (int i = 0; i < tasks.size(); i++) {
-                System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
+    private static void list() {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
+        }
+    }
+
+    private static void handleMark(String inp) {
+        Scanner s = new Scanner(inp);
+        s.next();
+        if (s.hasNextInt()) {
+            int idx = s.nextInt();
+            if (idx > tasks.size() || idx < 0) {
+                System.out.println("oh...no..waaaaa *cries invalid twask nwumber....");
             }
-        } else if (inp.startsWith("mark")) {
-            Scanner s = new Scanner(inp);
-            s.next();
-            if (s.hasNextInt()) {
-                int idx = s.nextInt();
-                if (idx > tasks.size() || idx < 0) {
-                    System.out.println("oh...no..waaaaa *cries invalid twask nwumber....");
-                }
-                tasks.get(idx).markDone();
-                System.out.println(String.format("marked item %d :D", idx));
+            tasks.get(idx).markDone();
+            System.out.println(String.format("marked item %d :D", idx));
+        } else {
+            System.out.println("pls gib task no. for me to mark uwu uwu");
+        }
+    }
+
+    private static void handleUnmark(String inp) {
+        Scanner s = new Scanner(inp);
+        s.next();
+        if (s.hasNextInt()) {
+            int idx = s.nextInt();
+            tasks.get(idx).unmarkDone();
+        }
+    }
+
+    private static void handleTodo(String inp) {
+        Matcher todoMatcher = TODO_PATTERN.matcher(inp);
+        if (todoMatcher.matches()) {
+            String details = todoMatcher.group(1);
+            Task newTask = new ToDo(details);
+            addToTasks(newTask);
+            System.out.println(String.format("watashi added the task %s !! anata have %d tasks to go!!",
+                    newTask, tasks.size()));
+        } else {
+            System.out.println("gib me something to work with !!! :(((");
+        }
+    }
+
+    private static void handleDeadline(String inp) {
+        Matcher deadlineMatcher = DEADLINE_PATTERN.matcher(inp);
+        if (deadlineMatcher.matches()) {
+            String details = deadlineMatcher.group(1);
+            String by = deadlineMatcher.group(2);
+            Task newTask = new Deadline(details, by);
+            addToTasks(newTask);
+            System.out.println(String.format("oh no scary deadlinw.... %s", newTask));
+        } else {
+            System.out.println("no pls gib the details and the deadline");
+        }
+    }
+
+    private static void handleEvent(String inp) {
+        Matcher eventMatcher = EVENT_PATTERN.matcher(inp);
+        if (eventMatcher.matches()) {
+            String details = eventMatcher.group(1);
+            String from = eventMatcher.group(2);
+            String to = eventMatcher.group(3);
+            Task newTask = new Event(details, from, to);
+            addToTasks(newTask);
+            System.out.println(String.format("yeeeeees event %s added", newTask));
+        } else {
+            System.out.println("nu !!!! gimme the deets the from the to");
+        }
+    }
+
+    public static void handleDelete(String inp) {
+        Scanner s = new Scanner(inp);
+        s.next();
+        if (s.hasNextInt()) {
+            int idx = s.nextInt();
+            if (idx > tasks.size() || idx < 0) {
+                System.out.println("inwalid index");
             } else {
-                System.out.println("pls gib task no. for me to mark uwu uwu");
-            }
-        } else if (inp.startsWith("unmark")) {
-            Scanner s = new Scanner(inp);
-            s.next();
-            if (s.hasNextInt()) {
-                int idx = s.nextInt();
-                tasks.get(idx).unmarkDone();
-            }
-        } else if (inp.startsWith("todo")) {
-            Matcher todoMatcher = TODO_PATTERN.matcher(inp);
-            if (todoMatcher.matches()) {
-                String details = todoMatcher.group(1);
-                Task newTask = new ToDo(details);
-                addToTasks(newTask);
-                System.out.println(String.format("watashi added the task %s !! anata have %d tasks to go!!",
-                        newTask, tasks.size()));
-            } else {
-                System.out.println("gib me something to work with !!! :(((");
-            }
-        } else if (inp.startsWith("deadline")) {
-            Matcher deadlineMatcher = DEADLINE_PATTERN.matcher(inp);
-            if (deadlineMatcher.matches()) {
-                String details = deadlineMatcher.group(1);
-                String by = deadlineMatcher.group(2);
-                Task newTask = new Deadline(details, by);
-                addToTasks(newTask);
-                System.out.println(String.format("oh no scary deadlinw.... %s", newTask));
-            } else {
-                System.out.println("no pls gib the details and the deadline");
-            }
-        } else if (inp.startsWith("event")) {
-            Matcher eventMatcher = EVENT_PATTERN.matcher(inp);
-            if (eventMatcher.matches()) {
-                String details = eventMatcher.group(1);
-                String from = eventMatcher.group(2);
-                String to = eventMatcher.group(3);
-                Task newTask = new Event(details, from, to);
-                addToTasks(newTask);
-                System.out.println(String.format("yeeeeees event %s added", newTask));
-            } else {
-                System.out.println("nu !!!! gimme the deets the from the to");
-            }
-        } else if (inp.startsWith("delete")) {
-            Scanner s = new Scanner(inp);
-            s.next();
-            if (s.hasNextInt()) {
-                int idx = s.nextInt();
-                if (idx > tasks.size() || idx < 0) {
-                    System.out.println("inwalid index");
-                } else {
-                    tasks.remove(idx);
-                    System.out.println("!!! begone you normie!!");
-                }
-            } else {
-                System.out.println("wub wub... no indewx....");
+                tasks.remove(idx);
+                System.out.println("!!! begone you normie!!");
             }
         } else {
-            System.out.println("eeek?? nani ??/");
+            System.out.println("wub wub... no indewx....");
+        }
+    }
+
+    private static void chat(Scanner sc) {
+        String inp = sc.nextLine();
+        Action action = Action.fromInput(inp);
+
+        switch (action) {
+            case BYE -> exit();
+            case LIST -> list();
+            case MARK -> handleMark(inp);
+            case UNMARK -> handleUnmark(inp);
+            case TODO -> handleTodo(inp);
+            case DEADLINE -> handleDeadline(inp);
+            case EVENT -> handleEvent(inp);
+            case DELETE -> handleDelete(inp);
+            default -> System.out.println("eeek?? nani ??/");
         }
     }
 }
