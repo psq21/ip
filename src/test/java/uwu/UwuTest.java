d06368ace7a8c2778fb6db294dec8b92adec4421
Phung SQ <phungsiqi@gmail.com>
2026-09-17 19:06:07 +0800

    Fix naming of chatbot in some file

--- src/test/java/uwu/UwuTest.java
+++ src/test/java/uwu/UwuTest.java
@@ -6,18 +6,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UwuTest {
     @Test
-    void getResponse_unknownCommand_returnsUnknownMessage() {
-        assertEquals("eeek?? nani ??/", Uwu.getResponse("what is this"));
+    void actionFromInput_recognisesCommands() {
+        assertEquals(Uwu.Action.BYE, Uwu.Action.fromInput("bye"));
+        assertEquals(Uwu.Action.LIST, Uwu.Action.fromInput("list"));
+        assertEquals(Uwu.Action.MARK, Uwu.Action.fromInput("mark 1"));
+        assertEquals(Uwu.Action.UNMARK, Uwu.Action.fromInput("unmark 1"));
+        assertEquals(Uwu.Action.TODO, Uwu.Action.fromInput("todo task"));
+        assertEquals(Uwu.Action.DEADLINE, Uwu.Action.fromInput("deadline task /by 2026-01-01"));
+        assertEquals(Uwu.Action.EVENT, Uwu.Action.fromInput("event task /from 2026-01-01 /to 2026-01-02"));
+        assertEquals(Uwu.Action.SORT, Uwu.Action.fromInput("sort"));
+        assertEquals(Uwu.Action.DELETE, Uwu.Action.fromInput("delete 1"));
     }
 
     @Test
-    void getResponse_bye_returnsExitMessage() {
-        assertEquals("gwooooooooddbyyeee seeeeee youuuuuuuu <3", Uwu.getResponse("bye"));
-    }
-
-    @Test
-    void actionFromInput_requiresExactMatchForListAndSort() {
-        assertEquals(Uwu.Action.UNKNOWN, Uwu.Action.fromInput("listing"));
-        assertEquals(Uwu.Action.UNKNOWN, Uwu.Action.fromInput("sort tasks"));
+    void actionFromInput_unknownCommand_returnsUnknown() {
+        assertEquals(Uwu.Action.UNKNOWN, Uwu.Action.fromInput("wat"));
     }
 }
